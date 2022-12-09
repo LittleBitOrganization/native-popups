799009375772acb1e2840b771deae28f67c4d4f9
Kate Kulikova <sunndaylive@gmail.com>
2022-12-09 15:28:58 +0300

    Add show basic alert for iOS

---
+++ Runtime/iOS/NativePopup.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace iOS
+{
+    public class NativePopup : MonoBehaviour
+    {
+        [DllImport("__Internal")]
+        private static extern void _ShowAlert(string title, string message);
+
+        public static void ShowAlert(string title, string message) => _ShowAlert(title, message);
+    }
+}
---
+++ Runtime/iOS/UIBindings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace iOS
+{
+    public class UIBindings : MonoBehaviour
+    {
+        [SerializeField] private Button showAlertButton;
+
+        private void Start()  
+        {
+            showAlertButton.onClick.AddListener(ShowBasicAlert);
+        }
+
+        private void ShowBasicAlert() 
+            => NativePopup.ShowAlert("Basic Alert", "Hello this is a basic alert !");
+    }
+}
